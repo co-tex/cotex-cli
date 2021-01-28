ab26a5bf48f965e32b88b9940a166eac0222d019
Sushovan Majhi <sush@smajhi.com>
2021-01-28 12:27:34 -0600

    add login

--- src/login.ts
+++ src/login.ts
@@ -1,12 +1,56 @@
 import * as ConfigStore from 'configstore';
 import * as CLI from 'clui';
+import jwtDecode, { JwtPayload } from "jwt-decode";
+import * as inquirer from 'inquirer';
+import { exit } from 'process';
+import axios from 'axios';
 
 const conf = new ConfigStore('@cotex/cli');
-const Spinner = CLI.Spinner;
-const status = new Spinner('Authenticating you, please wait...');
 
+const questions = [
+    {
+        name: 'email',
+        type: 'input',
+        message: 'Enter your CoTex e-mail address:',
+        validate: function(value: string) {
+            if (value.length) {
+              return true;
+            } else {
+              return 'Please enter your e-mail address.';
+            }
+        }
+    },
+    {
+        name: 'password',
+        type: 'password',
+        message: 'Enter your password:',
+        validate: function(value: string) {
+            if (value.length) {
+                return true;
+            } else {
+                return 'Please enter your password.';
+            }
+        },
+    }
+]
 
-function login() {
-    status.start();
+function login(): Promise<void> {
+    const token = conf.get('access_token');
+    if(token) {
+        const decoded = jwtDecode<JwtPayload>(token);
+        console.log('Already logged in!');
+        exit();
+    }
+    
+    return inquirer.prompt(questions)
+        .then(async (answers: any) => {
+            const status = new CLI.Spinner('Authenticating you. Please wait...');
+            status.start();
+
+            const response: any = await axios.post('http://localhost:3000/auth/login',answers);
+            status.stop();
+            console.log(response);
+            conf.set('access_token', response.data.access_token);
+        });
 }
 export { login as cli };
