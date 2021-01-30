43550e63bdbe6a2eda12bbfe1640323a03841f18
Sushovan Majhi <sush@smajhi.com>
2021-01-29 20:47:04 -0600

    add upload

--- src/sync.ts
+++ src/sync.ts
@@ -2,22 +2,36 @@ import axios from 'axios';
 import * as fs from 'fs';
 import * as dt from 'directory-tree';
 import * as diff from 'json-diff';
+import * as FormData from 'form-data';
 
 async function sync() {
   const remoteIdx = (await axios.get('http://localhost:3000/projects/1/index')).data;
-  const localIdx = dt('./', { exclude: /.cotex|.git|node_modules/, attributes: ['mtime'] });
-  const diffFiles = diff.diff(remoteIdx,localIdx);
+  const localIdx: any = {};
+  dt('./', { exclude: /.cotex|.git|node_modules/, attributes: ['mtime'] }, (file,path,stats) => {
+    localIdx[file.path] = file;
+  });
+  const diffFiles =  diff.diff(remoteIdx,localIdx);
+  
+  for(let fname in diffFiles) {
+    // upload
+    if(fname.split('__').pop() === 'added') {
+      const form = new FormData();
+      
+      const fileStream = fs.createReadStream('./' + diffFiles[fname].path)
+      const idx = diffFiles[fname].path.lastIndexOf(diffFiles[fname].name);
+      let path = '';
 
-  fs.writeFileSync('test', JSON.stringify(diffFiles, null, 2));
-  //console.log(diffFiles);
-  /* diffFiles.children.forEach((object: any) => {
-    axios.post('http://localhost:3000/1/sync', fs.readFileSync('./' + object.path),{
-      headers: {
-          filePath: object.name,
-          'Content-Type': 'multipart/form-data'
+      if(idx > 0 ) {
+        path = diffFiles[fname].path.slice(0,idx - 1);
       }
-    })
-  }); */
+      form.append('path', path);
+      form.append('file', fileStream);
+      
+      axios.post('http://localhost:3000/projects/1/sync', form,{
+        headers: form.getHeaders()
+      });
+    }
+  }
 }
 exports.api = sync;
 exports.cli = sync;
